540ff64f3574ebc05d410757670be11fb2e12c19
NexWan <l21051403@saltillo.tecnm.mx>
2023-05-06 13:23:01 -0600

    Added an extra font

--- IntroduccionPHP/Proyecto/php/menu/inicio.php
+++ IntroduccionPHP/Proyecto/php/menu/inicio.php
@@ -34,7 +34,7 @@
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&family=Sigmar+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&family=Sigmar+One&display=swap&family=Ubuntu&display=swap" rel="stylesheet">
     <title>Home</title>
 </head>
 <body class="inicio">
